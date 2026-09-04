7620b17959b29a7c297d81de63f4729b2416162a
moydom-bit <moydom@antalyamoydom.com.tr>
2026-09-04 14:18:13 +0300

    kalkan-tur2 yuklendi
    kalkan-tur2 yuklendi ve hazir.

---
+++ data.js
@@ -0,0 +1,50 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-villa-for-sale-in-ortaalan-kalkan",
+      "name": "villa for sale in Ortaalan Kalkan",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
